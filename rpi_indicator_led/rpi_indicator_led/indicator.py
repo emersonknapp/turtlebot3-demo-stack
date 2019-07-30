3c885d8441c6d84c0acde1b5a3d46483935a8ff7
Emerson Knapp <537409+emersonknapp@users.noreply.github.com>
2019-07-30 12:03:28 -0700

    Indicator LED node (#1)
    * Add new rpi_indicator_led package

---
+++ rpi_indicator_led/rpi_indicator_led/indicator.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from gpiozero import PWMLED, LED
+
+import rclpy
+from rclpy.node import Node
+
+OUTPUT_PIN = 18
+POWER_PIN = 15
+
+
+class IndicatorNode(Node):
+    def __init__(self, led):
+        super().__init__('indicatorled')
+        self.led = led
+        self.power = LED(POWER_PIN)
+        self.power.on()
+        self.startup_pulse()
+        self.create_timer = self.create_timer(3.0, self.blink)
+
+    def startup_pulse(self):
+        self.led.pulse(fade_in_time=1, fade_out_time=0, n=1, background=False)
+        self.led.value = 1
+
+    def blink(self):
+        self.led.blink(on_time=0.1, off_time=0.08, n=2, background=False)
+        self.led.value = 1
+
+
+def main(args=None):
+    led = PWMLED(OUTPUT_PIN)
+    rclpy.init(args=args)
+    node = IndicatorNode(led)
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+    led.value = 0
+
+
+if __name__ == '__main__':
+    main()
